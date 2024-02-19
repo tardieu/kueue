20727dca9f06c28ae6f255178720cac39ac8faa9
Shilpa Chugh <shchugh@shchugh-thinkpadt14sgen1.remote.csb>
2024-02-19 16:47:10 +0530

    PATCH:Change func to wait until the admission check is properly removed

--- test/e2e/singlecluster/e2e_test.go
+++ test/e2e/singlecluster/e2e_test.go
@@ -357,7 +357,7 @@ var _ = ginkgo.Describe("Kueue", func() {
 			gomega.Expect(util.DeleteAllJobsInNamespace(ctx, k8sClient, ns)).Should(gomega.Succeed())
 			util.ExpectClusterQueueToBeDeleted(ctx, k8sClient, clusterQueue, true)
 			util.ExpectResourceFlavorToBeDeleted(ctx, k8sClient, onDemandRF, true)
-			gomega.Expect(k8sClient.Delete(ctx, check)).Should(gomega.Succeed())
+			util.ExpectAdmissionCheckToBeDeleted(ctx, k8sClient, check, true)
 		})
 
 		ginkgo.It("Should unsuspend a job only after all checks are cleared", func() {
